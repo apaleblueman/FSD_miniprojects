944ad5f551b9b3abca4d6d5dc1ce108dfb9fcbca
apaleblueman <gurchetan.singh2@icloud.com>
2026-01-31 16:20:06 +0530

    ADD: fully working MVP

--- SVGdrawingtool/script.js
+++ SVGdrawingtool/script.js
@@ -1,24 +1,37 @@
-    const canvas= document.getElementById('canvas');
-    const polyline = document.getElementById('pen')
-    let pendown, currentCoordinates, newCoordinates;
-    canvas.addEventListener('mousedown', function(event){
-            pendown=true;
-        });
-    canvas.addEventListener('mousemove', function(event){
-            if(pendown === true){
-                let rect = canvas.getBoundingClientRect();
-                let x = event.clientX - rect.left;
-                let y = event.clientY - rect.top;
-                let coor = y +" "+ x ;
-                // let coor = x + " " + y;
-                // console.log(coor);
-                currentCoordinates = polyline.getAttribute("points");
-                newCoordinates = currentCoordinates +","+ coor;
-                
-                polyline.setAttribute("points", newCoordinates);
-            }
-        });
-    canvas.addEventListener('mouseup', function(){
-        pendown=false;  
-    })
-    
+const canvas= document.getElementById('canvas');
+
+let pendown, currentCoordinates, newCoordinates,newline;
+canvas.addEventListener('mousedown', function(event){
+    pendown=true;
+    newline = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+    let coor = x + " " + y;
+
+    newline.setAttribute("points", coor);
+    canvas.appendChild(newline);
+
+            
+    });
+canvas.addEventListener('mousemove', function(event){
+        if(pendown === true){
+            let rect = canvas.getBoundingClientRect();
+            let x = event.clientX - rect.left;
+            let y = event.clientY - rect.top;
+            let coor = x +" "+ y ;
+            // let coor = x + " " + y;
+            // console.log(coor);
+            let currentCoordinates = newline.getAttribute("points");
+            
+            let newCoordinates = currentCoordinates + "," + coor;
+            console.log(currentCoordinates, newCoordinates);
+
+            newline.setAttribute("style", "fill:none;stroke:black");
+            newline.setAttribute("points", newCoordinates);
+            
+        }
+    });
+canvas.addEventListener('mouseup', function(){
+    pendown=false;  
+})
