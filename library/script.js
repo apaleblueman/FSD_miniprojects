f215682b6d947d2d170ce7921f3fa2b47958af0f
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-02-22 11:12:46 +0530

    ADD: delete button functionality

--- library/script.js
+++ library/script.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+let myLibrary = [];
 function Book(name, author, pages, read) {
     if(!(new.target)){
         console.log("use new keyword when creating objects using constructor!");
@@ -29,6 +29,7 @@ function displayBooks(){
         // console.log(`Book ID${bookObj.id}`);
         const card = document.createElement("div");
         card.className = "card";
+        card.setAttribute('id', bookObj.id);
         const bookname = document.createElement("h1"); 
         const author = document.createElement("h3"); 
         const pages = document.createElement("span"); 
@@ -37,6 +38,9 @@ function displayBooks(){
         const icon = document.createElement("img");
         icon.setAttribute("src", "./delete.svg");
         remove.className = "delete";
+        // remove.id = 'delete';
+        remove.setAttribute("id", "delete");
+
         remove.appendChild(icon);
 
         bookname.textContent = bookObj.name;
@@ -50,21 +54,34 @@ function displayBooks(){
         card.appendChild(read);
         card.appendChild(remove);
         contentWindow.appendChild(card)
-
+        
+        icon.addEventListener('click', function(){
+            myLibrary = myLibrary.filter(book => book.id !== bookObj.id);
+            card.remove();
+        })
     });
+
 }
 
 document.getElementById('addbook').addEventListener('click', function(e){
     const name = document.getElementById('name').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
-    const read = document.getElementById('read').value;
+    const read = document.getElementById('read').checked;
+    let read_line;
+    if(read == true){
+        read_line = "read";
+    }
+    else{
+        read_line = "not read yet"
+    }
     // const name = "test";
     // const author = "test";
     // const pages = "test";
     // const read = "test";
-    addBookToLibrary(name, author, pages, read);
+    addBookToLibrary(name, author, pages, read_line);
     displayBooks();
     e.preventDefault();
-
+    
 })
+
